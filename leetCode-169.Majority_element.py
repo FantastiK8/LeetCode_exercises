5990d18eed37eb6514c8c2824e78f200694e6615
FantastiK8 <katerina.montoban@gmail.com>
2021-12-10 23:44:05 +0000

    Uploading of successful solution of leetCode 169 Majority Element.

--- leetCode-169.Majority_element.py
+++ leetCode-169.Majority_element.py
@@ -26,10 +26,37 @@ n == nums.length
 Follow-up: Could you solve the problem in linear time and in O(1) space?
 """
 
+# Success
+# Details 
+# Runtime: 132 ms, faster than 90.56% of Python online submissions for Majority Element.
+# Memory Usage: 14.8 MB, less than 48.38% of Python online submissions for Majority Element.
+
+
 class Solution(object):
     def majorityElement(self, nums):
         """
         :type nums: List[int]
         :rtype: int
         """
+        lenght_nums = len(nums)
+        majority_min = len(nums)/2
+        dict = {}
+
+
+        for num in nums:
+            if num in dict:
+                dict[num] += 1
+            else:
+                 dict[num] = 1
         
+        for num in dict:
+            if dict[num] > majority_min:
+                return num
+
+
+
+
+nums = [2,2,1,1,1,2,2]
+nums = [3,2,3]
+sol = Solution()
+print(sol.majorityElement(nums))
